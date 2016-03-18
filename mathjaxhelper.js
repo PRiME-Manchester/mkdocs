7ad296bf13a29558e5382c1b3a63766dd0febe72
Patrick Camilleri <patrick.camilleri@manchester.ac.uk>
2016-03-18 17:02:31 +0100

    Deployed 7a309f6 with MkDocs version: 0.15.3

---
+++ mathjaxhelper.js
@@ -0,0 +1,15 @@
+/*
+#if false
+Prevent Unity to try compiling this js
+*/
+MathJax.Hub.Config
+	({
+	"tex2jax":
+		{
+		inlineMath: [ [ '$', '$' ] ],
+		processEscapes: true
+		}
+	});
+/*
+#endif
+*/
